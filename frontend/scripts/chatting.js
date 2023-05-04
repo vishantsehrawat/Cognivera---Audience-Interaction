bdcb561010ea52b6bee4eedf87371a1495d16415
vishant <vishant.96.sehrawat@gmail.com>
2023-05-05 03:32:59 +0530

    completed chatting page , fetching username remaining

---
+++ frontend/scripts/chatting.js
@@ -0,0 +1,70 @@
+const socket = io("http://localhost:8080/", { transports: ["websocket"] });
+
+let username = "abhimanyu" //Getting it from somewhere //Cache //LS etc. 
+
+// For checking connection on frontend
+socket.on("start", (message) => {
+    console.log("connected to socket server");
+})
+
+//Count tag update
+socket.on("newuser", (msg) => {
+    const count_tag = document.getElementById("usercount")
+    count_tag.innerText = msg
+
+})
+
+// ^ Handling Receiving Messages
+socket.on("usermsg", (message) => {
+
+    const messages = document.querySelector('#messages')
+
+    let otherdiv = document.createElement("div")
+    otherdiv.setAttribute("class", "otherdiv")
+
+    let innerdiv = document.createElement("div")
+    innerdiv.setAttribute("class", "innerdiv")
+
+    let name = document.createElement("h4")
+    name.innerText = message.name
+    let msg = document.createElement("p")
+    msg.innerText = message.message
+
+
+    innerdiv.append(name, msg)
+    otherdiv.append(innerdiv)
+    messages.append(otherdiv)
+
+})
+
+
+
+//On Click function on send button to display own msg and emmit to others
+const sendMessage = (event) => {
+    const textBox = document.getElementById("input")
+    const text = document.getElementById("input").value
+    const message = { name: username, message: text }
+    socket.emit("message", message)// emitting message on enter
+
+    // ^ below code is to append message on sender side 
+    const messages = document.querySelector('#messages')
+
+    let mydiv = document.createElement("div")
+    mydiv.setAttribute("class", "mydiv")
+
+    let innerdiv = document.createElement("div")
+    innerdiv.setAttribute("class", "innerdiv")
+
+    let name = document.createElement("h4")
+    name.innerText = username
+    let msg = document.createElement("p")
+    msg.innerText = text
+
+    innerdiv.append(name, msg)
+    mydiv.append(innerdiv)
+    messages.append(mydiv)
+
+    // to clear input box 
+    textBox.value = "";
+}
+
