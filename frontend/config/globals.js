1fa5016aa8ed2d880c685c9ba66af68df35d3668
vishant <vishant.96.sehrawat@gmail.com>
2023-08-03 02:50:10 +0530

    gitignore changes for globals file

---
+++ frontend/config/globals.js
@@ -0,0 +1,10 @@
+
+// globals.js
+const globals = {
+
+    // use when working with the deployed url
+    DEPLOYED_URL  :"https://slidoapp.onrender.com"
+
+    // use when working with localhost
+    // DEPLOYED_URL: "http://localhost:8080",
+};
